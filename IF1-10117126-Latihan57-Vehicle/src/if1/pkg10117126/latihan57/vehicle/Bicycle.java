68a211b4edc109e735800768f23280fe70f0a48d
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-11-20 17:29:03 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan57-Vehicle/src/if1/pkg10117126/latihan57/vehicle/Bicycle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan57.vehicle;
+
+/**
+ *
+ * @author MAWA
+ */
+public class Bicycle extends Vehicle {
+
+    private int myGearCount;
+
+    public Bicycle() {
+        System.out.println("Bicycle");
+    }
+
+    public int getGearCount() {
+        return myGearCount;
+    }
+
+    public void setGearCount(int myGearCount) {
+        this.myGearCount = myGearCount;
+    }
+
+}
---
+++ IF1-10117126-Latihan57-Vehicle/src/if1/pkg10117126/latihan57/vehicle/Skateboard.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117126.latihan57.vehicle;
+
+/**
+ *
+ * @author MAWA
+ */
+public class Skateboard extends Vehicle {
+
+    private double myBoardLeght;
+
+    public Skateboard() {
+        System.out.println("Skateboard");
+    }
+
+    public double getBoardLeght() {
+        return myBoardLeght;
+    }
+
+    public void setBoardLeght(double myBoardLeght) {
+        this.myBoardLeght = myBoardLeght;
+    }
+}
